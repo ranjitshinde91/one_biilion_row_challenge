28979f660005ad6fa947c8f0469eabefb3643f51
Ranjit Shinde <r.shinde.ext@sportradar.com>
2024-04-28 15:02:10 +0530

    makes application read fileName from command line and parse

--- src/main/java/demo/practice/Main.java
+++ src/main/java/demo/practice/Main.java
@@ -4,11 +4,26 @@ import demo.practice.parser.OneBillionRowParser;
 import demo.practice.parser.WeatherResult;
 
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+
+    while (true) {
+      System.out.println(" Enter file name: ");
+      Scanner sc = new Scanner(System.in);
+      String fileName = sc.next();
+      if (fileName.equals("exit")) {
+        break;
+      }
+      parseFile(fileName);
+    }
+    System.out.println("application stopped");
+  }
+
+  private static void parseFile(String fileName) {
     var start = System.currentTimeMillis();
-    OneBillionRowParser oneBillionRowParser = new OneBillionRowParser(args[0]);
+    OneBillionRowParser oneBillionRowParser = new OneBillionRowParser(fileName);
     Map<String, WeatherResult> parse = oneBillionRowParser.parse();
     var end = System.currentTimeMillis();
     System.out.println("Time taken in millis: " + (end - start));
